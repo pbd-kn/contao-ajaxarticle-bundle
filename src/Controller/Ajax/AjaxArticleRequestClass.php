b48d8db1331d20accb9d3e60ee34ac9467814760
pbd-kn <75879364+pbd-kn@users.noreply.github.com>
2022-10-11 13:47:53 +0200

    modal css uebenommen

--- src/Controller/Ajax/AjaxArticleRequestClass.php
+++ src/Controller/Ajax/AjaxArticleRequestClass.php
@@ -4,26 +4,23 @@ declare(strict_types=1);
 
 namespace PBDKN\AjaxArticleBundle\Controller\Ajax;
 
-
-
 use Contao\ContentModel;
 use Contao\CoreBundle\Framework\ContaoFramework;
+use Contao\CoreBundle\InsertTag\InsertTagParser;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Contao\FilesModel;
 use Contao\StringUtil;
 use Doctrine\DBAL\Connection;
 use Doctrine\DBAL\Driver\Exception as DoctrineDBALDriverException;
 use Doctrine\DBAL\Exception as DoctrineDBALException;
-//use Markocupic\GalleryCreatorBundle\Model\GalleryCreatorAlbumsModel;
-//use Markocupic\GalleryCreatorBundle\Model\GalleryCreatorPicturesModel;
-//use Markocupic\GalleryCreatorBundle\Util\AlbumUtil;
-//use Markocupic\GalleryCreatorBundle\Util\PictureUtil;
-//use Markocupic\GalleryCreatorBundle\Util\SecurityUtil;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Contracts\Translation\TranslatorInterface;
-class AjaxArticleRequestClass
+
+
+class AjaxArticleRequestClass extends AbstractController
 {
     private ContaoFramework $framework;
     private Connection $connection;
@@ -31,13 +28,15 @@ class AjaxArticleRequestClass
      * @var TranslatorInterface
      */
     private $translator;
+    private InsertTagParser $insertTagParser;
+
         
-    // constructor wird mittels  autowire: true in services.yaml gesetzt
-    public function __construct(ContaoFramework $framework, Connection $connection,TranslatorInterface $translator)
+    public function __construct(ContaoFramework $framework, Connection $connection,TranslatorInterface $translator,InsertTagParser $insertTagParser)
     {
         $this->framework = $framework;
         $this->connection = $connection;
         $this->translator = $translator;
+        $this->insertTagParser = $insertTagParser;
     }
     
     /**
@@ -51,6 +50,7 @@ class AjaxArticleRequestClass
 	public function getArticleAjax(string $article)
 	{
         global $GLOBALS;
+
         $this->framework->initialize(true);
         $objArticleModel = \ArticleModel::findByIdOrAliasAndPid($article, null);
         $id=$objArticleModel->id;
@@ -60,20 +60,23 @@ class AjaxArticleRequestClass
         $pObj = \PageModel::findByPk($pid);
         $GLOBALS['objPage'] = $pObj;                    // bei alten modules z.b.efg mit formulareingabe
 	
-		if (($strOutput = \Controller::getArticle($objArticleModel, false, true)) !== false){ 
-          $art = ltrim($strOutput);
-		} else {
-            $trans=$this->translator->trans('MSC.invalidPage', [], 'contao_default');
-			$art = '<p class="error">' . sprintf($trans, $article) . '</p>';
-		}
-
-        $art1 = str_replace(                 // Umlaute ersetzen
-           ['ä','ö','ü','Ä','Ö','Ü','ß'],
-           ['&auml;','&ouml;','&uuml;','&Auml;','&Ouml;','&Uuml;','&szlig;'],
-           $art
-        ); 
-        //$art1=htmlspecialchars($art1);       
-		return new JsonResponse(['article' => $article,'id'=>$id,'alias' => $alias,'title' =>$title,'artikel'=>$art1]); 
+        $strOutput =  $this->insertTagParser->replaceInline("{{insert_article::$alias}}");
+        $pos = strpos($strOutput, "error");
+        $art="";
+        if($pos !== FALSE) {
+          $invP=$this->translator->trans('MSC.invalidPage', [], 'contao_default');
+          $invA=$this->translator->trans('CTE.article.0', [], 'contao_default');
+          $invAl=$this->translator->trans('CTE.alias.0', [], 'contao_default');
+          
+		  $art = '<p class="error">' . sprintf($invP, $article) . '</p>';
+          $art .= "<p>$invA: $article<br>$invAl(alias): $alias  </p>";
+        } else {
+          $art =  ltrim($strOutput);
+        }
+        $art = utf8_encode($art);         // json geht nur ordentlich mit utf-8
+        
+		$response = new JsonResponse(['article' => $article,'id'=>$id,'alias' => $alias,'title' =>$title,'artikel'=>$art]); 
+		return $response; 
 
 	}
 }
